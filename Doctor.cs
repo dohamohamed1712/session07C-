1837cbd12aaa474fbb3a7d8076e83caee1b820fa
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:47:37 +0300

    Doctor.cs

--- Doctor.cs
+++ Doctor.cs
@@ -1,10 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Doctor
+    internal class Doctor : Person
     {
+        public string Specialty { get; set; }
+
+        public Doctor(int id, string name, int age, string specialty) : base(id, name, age)
+        {
+            Specialty = specialty;
+        }
+
+        
+        public new void Greet()
+        {
+            Console.WriteLine("I am a Doctor.");
+        }
+
+       
+        public override void Display()
+        {
+            Console.WriteLine("ID: " + ID + ", Name: " + Name + ", Age: " + Age + ", Specialty: " + Specialty);
+        }
     }
 }
