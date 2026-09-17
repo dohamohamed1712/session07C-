0853f38328a02676af81d4b727ba5db7deca8a9f
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:50:04 +0300

    Person.cs

--- Person.cs
+++ Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,27 @@ namespace session07C_
 {
     internal class Person
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("I am a Person.");
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("ID: " + ID + ", Name: " + Name + ", Age: " + Age);
+        }
+
+
     }
 }
