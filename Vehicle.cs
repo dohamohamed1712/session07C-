0fbb148b2ef67dd933e2e5edd98cd739b3041c28
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:51:39 +0300

    Vehicle.cs

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,10 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Vehicle
+    internal class Vehicle:IVehicle
     {
+        public virtual void MoveForward()
+        {
+            Console.WriteLine("Vehicle is moving forward.");
+        }
+
+        public virtual void MoveBackward()
+        {
+            Console.WriteLine("Vehicle is moving backward.");
+        }
+
+        public virtual void MoveUp()
+        {
+            Console.WriteLine("Vehicle is moving up.");
+        }
+
+        public virtual void MoveDown()
+        {
+            Console.WriteLine("Vehicle is moving down.");
+        }
     }
 }
