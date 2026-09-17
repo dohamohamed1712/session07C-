9cd6432ec47bc026fed44ca24b136df1aa3b3f80
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:50:51 +0300

    Shape.cs

--- Shape.cs
+++ Shape.cs
@@ -6,5 +6,24 @@ namespace session07C_
 {
     internal class Shape
     {
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Shape(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public override string ToString()
+        {
+            return "(Width = " + Width + ", Height = " + Height + ")";
+        }
     }
 }
