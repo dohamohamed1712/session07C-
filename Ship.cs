13456e54beb408dea155ea1a282bbc1b4c63dc56
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:51:16 +0300

    Ship.cs

--- Ship.cs
+++ Ship.cs
@@ -1,10 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Ship
+    internal class Ship :IMoveable
     {
+        void IMoveable.MoveForward()
+        {
+            Console.WriteLine("Ship is moving forward on the sea. (Explicit)");
+        }
+
+
+        public void MoveBackward()
+        {
+            Console.WriteLine("Ship is moving backward on the sea.");
+        }
     }
 }
