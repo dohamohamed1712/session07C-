61544e53931edb7e10ddd5e8316b14e1720775dd
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:46:13 +0300

    Car.cs

--- Car.cs
+++ Car.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Car
+    internal class Car : IMoveable
     {
+        public void MoveForward()
+        {
+            Console.WriteLine("Car is moving forward on the ground.");
+        }
+
+        public void MoveBackward()
+        {
+            Console.WriteLine("Car is moving backward on the ground.");
+        }
     }
 }
