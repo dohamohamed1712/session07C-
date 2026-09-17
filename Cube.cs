5dcab74c680703ecef3eb74c7aa3cfe7fc90c5b3
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:46:58 +0300

    Cube.cs

--- Cube.cs
+++ Cube.cs
@@ -1,10 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Cube
+    internal class Cube:Shape
     {
+        public double Depth { get; set; }
+
+        public Cube(double width, double height, double depth) : base(width, height)
+        {
+            Depth = depth;
+        }
+
+       
+        public new double Area()
+        {
+            return base.Area() * Depth;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Width = " + Width + ", Height = " + Height + ", Depth = " + Depth);
+        }
     }
 }
