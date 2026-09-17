131abcdba128f5fdd6ed99f0e9abf83e723b2006
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:52:55 +0300

    Add files via upload

---
+++ IMoveable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace session07C_
+{
+    internal interface IMoveable
+    {
+        void MoveForward();
+        void MoveBackward();
+    }
+}
