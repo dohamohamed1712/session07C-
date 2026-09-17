cb22b0c604d9dc73a8b4f8aac5f7caa07d930d18
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:45:29 +0300

    Airplane.cs

--- Airplane.cs
+++ Airplane.cs
@@ -1,10 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Airplane
+    internal class Airplane :IMoveable ,IFlyable
     {
+        public void MoveForward()
+        {
+            Console.WriteLine("Airplane is moving forward in the air.");
+        }
+
+        public void MoveBackward()
+        {
+            Console.WriteLine("Airplane is moving backward in the air.");
+        }
+
+        public void MoveUp()
+        {
+            Console.WriteLine("Airplane is moving up.");
+        }
+
+        public void MoveDown()
+        {
+            Console.WriteLine("Airplane is moving down.");
+        }
     }
 }
