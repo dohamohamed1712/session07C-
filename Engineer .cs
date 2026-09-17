6747c5e3a2fa7be4c15eeb65e9857e7131e489b0
Doha Mohamed <dohamohamed1712@gmail.com>
2026-09-17 13:48:14 +0300

    Engineer .cs

--- Engineer .cs
+++ Engineer .cs
@@ -1,10 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace session07C_
 {
-    internal class Engineer
+    internal class Engineer :Person
     {
+        public string Field { get; set; }
+        public int YearsOfExperience { get; set; }
+
+        public Engineer(int id, string name, int age, string field, int yearsOfExperience) : base(id, name, age)
+        {
+            Field = field;
+            YearsOfExperience = yearsOfExperience;
+        }
+
+       
+        public new void Greet()
+        {
+            Console.WriteLine("I am an Engineer.");
+        }
+
+        
+        public override void Display()
+        {
+            Console.WriteLine("ID: " + ID + ", Name: " + Name + ", Age: " + Age + ", Field: " + Field + ", Years of Experience: " + YearsOfExperience);
+        }
     }
 }
